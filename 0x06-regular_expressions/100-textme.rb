3de5bb1b07cec14eea46fd5a5ee2d2949f8a6570
SebastianBlandon <blandonodz@gmail.com>
2022-06-28 13:45:20 -0500

    [UPDATED] 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-
-puts ARGV[0].scan(/(?<=from:).+?(?=\])/).join
-puts ARGV[0].scan(/(?<=to:).+?(?=\])/).join
-puts ARGV[0].scan(/(?<=flags:).+?(?=\])/).join
+puts ARGV[0].scan(/(?<=from:|to:|flags).+?(?=\]),/).join
