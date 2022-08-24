1ea29a10896eaaca0c81731653b4376eb749197c
SebastianBlandon <blandonodz@gmail.com>
2022-08-23 23:52:23 -0500

    [ADDED] 100-puppet_ssh_config.pp file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Turn off passwd auth
+file_line { 'Turn off passwd auth':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '#   PasswordAuthentication no'
+}
+
+# Declare identity file
+file_line { 'Declare identity file':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '#   IdentityFile ~/.ssh/school'
+}
