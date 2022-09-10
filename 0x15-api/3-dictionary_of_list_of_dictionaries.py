938db9fac18f3029b763a6316b86c5a4b4891a11
SebastianBlandon <blandonodz@gmail.com>
2022-09-09 21:22:56 -0500

    [UPDATED] pycodestyle 3-dictionary_of_list_of_dictionaries.py file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
 """
-    Export to JSON
+    Dictionary of list of dictionaries
     Using what you did in the task #0,
     extend your Python script to export data in the JSON format.
 """
 
 import json
 from requests import get
-from sys import argv, exit
 
 if __name__ == "__main__":
-    try:
-        id = argv[1]
-        is_integer = int(id)
-    except Exception:
-        exit()
-
     url = "https://jsonplaceholder.typicode.com/"
-    url_user = url + "users?id=" + id
-    url_todo = url + "todos?userId=" + id
+    url_user = url + "users"
+    url_todo = url + "todos"
 
     request_user = get(url_user)
     request_todo = get(url_todo)
@@ -37,12 +30,9 @@ if __name__ == "__main__":
             json_re[USER_ID] = []
             user_names[USER_ID] = USERNAME
 
-        # create the value of the dict of the final json file
-        # jslist = jsresult[USER_ID]
         for task in json_todo:
             TASK_TITLE = task.get('title')
             TASK_COMPLETED_STATUS = task.get('completed')
-            # write the internal dict
             user_id = task.get("userId")
             taskdict = {"task": TASK_TITLE,
                         "completed": TASK_COMPLETED_STATUS,
@@ -51,6 +41,5 @@ if __name__ == "__main__":
             if json_re.get(user_id) is not None:
                 json_re.get(user_id).append(taskdict)
 
-        # generate the jsonfile
         with open('todo_all_employees.json', 'w', newline='') as jsonfile:
             json.dump(json_re, jsonfile)
