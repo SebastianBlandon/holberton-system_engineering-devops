e9e3e91ec449d79ba277358f3f7883089c953720
SebastianBlandon <blandonodz@gmail.com>
2022-09-10 11:12:08 -0500

    [ADDED] 0-subs.py and anothers files

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    How many subs?
+    function that queries the Reddit API and returns the number of subscribers
+"""
+import requests
+
+url = 'https://www.reddit.com/'
+
+
+def number_of_subscribers(subreddit):
+    path = 'r/{}/about/.json'.format(subreddit)
+
+    response = requests.get("{}{}".format(url, path), allow_redirects=False)
+
+    if response.status_code != 200:
+        response.close()
+        return 0
+
+    data = response.json()
+    response.close()
+
+    subs = data.get('data').get('subscribers')
+
+    return subs
